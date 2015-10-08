4b5ee3e459605407e6758bc6317a8f0790b184ec
Jan M <jan.morez@gmail.com>
2015-10-08 12:04:24 +0200

    Semi-working cropPoints

--- cropPoints.m
+++ cropPoints.m
@@ -3,10 +3,11 @@ function quadData_Cropped=cropPoints(quadData,percentile)
     %<percentile> of all points. It will then remove all points outside a 
     %cylinder with said radius and make sure the face-data is not corrupted due
     %to the removing of points.
-
-    d=zeros(length(quadData),1);
+    n=length(quadData);
+    d=zeros(n,1);
+    
     %Calculate (cylindrical) distance for all points.
-    for j=1:length(quadData)
+    for j=1:n
         d(j)=sqrt(quadData(j,2)^2+quadData(j,3)^2);
     end
     [counts,centers]=hist(d(:),100);
@@ -17,8 +18,8 @@ function quadData_Cropped=cropPoints(quadData,percentile)
     %Next we remove all points that are outside this radius. Because this
     %will change the indices, we will have to remember this change and
     %apply it to the face-data.
-    keep_vertex=ones(length(quadData),1);
-    for j=1:length(quadData)
+    keep_vertex=ones(n,1);
+    for j=1:n
         if(d(j) > radius)
             keep_vertex(j)=0;
         end
@@ -29,23 +30,27 @@ function quadData_Cropped=cropPoints(quadData,percentile)
     %mapping=mapping(keep_vertex);
 
     %Remove vertices with the appropriate flag.
-    quadData_Cropped=quadData;%(keep_vertex==1,:);
+    quadData_Cropped=quadData(keep_vertex==1,:);
+    %Extract quad indices for remapping
+    Q=quadData_Cropped(:,5:8)+1;
+    Q_remapped=zeros(size(Q));  
     
-    %If a vertex is related to a face with a removed point, set the face
-    %data to -1 so it gets ignored when exporting. I still have to relabel
-    %all the quad indices, is that possible in O < O(n^2)??
-    
-    
-    Q_flat=quadData(:,4:8);
-    Q_flat=Q_flat(:);
-    for j=1:length(Q_flat)
-        Q_flat(j)=Q_flat(j)mapping(Q_flat(j));
-    end
-    
-    
-    for j=1:length(quadData_Cropped);
-        if ~keep_vertex(j)
-            quadData_Cropped(j,4:8)=-1*ones(1,4);
+    %Remap, if the quad references a deleted point, set the data to -1 so
+    %it gets ignored when exporting
+    for j=1:length(Q)
+        if(Q(j,1)~=0)
+            Q_remapped(j,1)=mapping(Q(j,1))*keep_vertex(Q(j,1))-(~keep_vertex(Q(j,1)));
+        end
+        if(Q(j,2)~=0)
+            Q_remapped(j,2)=mapping(Q(j,2))*keep_vertex(Q(j,2))-(~keep_vertex(Q(j,2)));
+        end
+        if(Q(j,3)~=0)
+            Q_remapped(j,3)=mapping(Q(j,3))*keep_vertex(Q(j,3))-(~keep_vertex(Q(j,3)));
+        end
+        if(Q(j,4)~=0)
+            Q_remapped(j,4)=mapping(Q(j,4))*keep_vertex(Q(j,4))-(~keep_vertex(Q(j,4)));
         end
     end
+    quadData_Cropped(:,5:8)=Q_remapped-1;
+
 end
